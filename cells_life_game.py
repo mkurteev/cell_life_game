a4d089ae302dc8fcc7cbf53f64121acd544c560f
Max Kurt <m.kurteev@gmail.com>
2024-06-24 18:23:11 +0300

    Create new project - Cell's Life

---
+++ cells_life_game.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+
+def main():
+    window = Tk()
+    window.title("Wild Max's Snake")
+    window.resizable(False,False)
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
